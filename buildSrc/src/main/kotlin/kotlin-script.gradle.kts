362ed955118a4067ca2c4ce6285310f688b98a48
Anton <anton.wiblishauser@gmx.de>
2022-11-30 20:28:43 +0100

    add core-api dependency

--- buildSrc/src/main/kotlin/kotlin-script.gradle.kts
+++ buildSrc/src/main/kotlin/kotlin-script.gradle.kts
@@ -6,9 +6,22 @@ plugins {
 
 repositories {
     mavenCentral()
+    maven("https://repo.dustrean.net/snapshots") {
+        credentials {
+            username = findProperty("dustreanUsername") as String
+            password = findProperty("dustreanPassword") as String
+        }
+    }
+    maven("https://repo.dustrean.net/releases") {
+        credentials {
+            username = findProperty("dustreanUsername") as String
+            password = findProperty("dustreanPassword") as String
+        }
+    }
 }
 
 dependencies {
+    implementation("net.dustrean:api:${BuildConstants.coreVersion}")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${BuildConstants.coroutinesVersion}")
 }
 
