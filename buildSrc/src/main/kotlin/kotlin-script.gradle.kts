646fd734e2505486caf3f6de08c713a8e843fe50
Anton <anton.wiblishauser@gmx.de>
2022-12-04 14:35:53 +0100

    fix dustrean repo

--- buildSrc/src/main/kotlin/kotlin-script.gradle.kts
+++ buildSrc/src/main/kotlin/kotlin-script.gradle.kts
@@ -8,14 +8,14 @@ repositories {
     mavenCentral()
     maven("https://repo.dustrean.net/snapshots") {
         credentials {
-            username = findProperty("dustreanUsername") as String
-            password = findProperty("dustreanPassword") as String
+            username = findProperty("DUSTREAN_REPO_USERNAME") as String? ?: System.getenv("DUSTREAN_REPO_USERNAME")
+            password = findProperty("DUSTREAN_REPO_PASSWORD") as String? ?: System.getenv("DUSTREAN_REPO_PASSWORD")
         }
     }
     maven("https://repo.dustrean.net/releases") {
         credentials {
-            username = findProperty("dustreanUsername") as String
-            password = findProperty("dustreanPassword") as String
+            username = findProperty("DUSTREAN_REPO_USERNAME") as String? ?: System.getenv("DUSTREAN_REPO_USERNAME")
+            password = findProperty("DUSTREAN_REPO_PASSWORD") as String? ?: System.getenv("DUSTREAN_REPO_PASSWORD")
         }
     }
 }
