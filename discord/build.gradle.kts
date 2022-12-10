70e419bc2489d5fc4e0928d8da3f165efb8c03d9
Anton <anton.wiblishauser@gmx.de>
2022-12-10 15:58:38 +0100

    create libloader config

--- discord/build.gradle.kts
+++ discord/build.gradle.kts
@@ -1,4 +1,10 @@
+import net.dustrean.libloader.plugin.LibraryLoader
+
 plugins {
     `kotlin-script`
     `discord-script`
+}
+
+the(LibraryLoader.LibraryLoaderConfig::class).apply {
+    mainClass.set("net.dustrean.modules.discord.DiscordModuleMainKt")
 }
