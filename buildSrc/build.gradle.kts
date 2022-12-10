5f346b0898243259e866c190a8669536f017c41f
Anton <anton.wiblishauser@gmx.de>
2022-12-10 15:58:17 +0100

    add libloadeer

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -5,11 +5,25 @@ plugins {
 repositories {
     gradlePluginPortal()
     maven("https://papermc.io/repo/repository/maven-public/")
+    maven("https://repo.dustrean.net/snapshots") {
+        credentials {
+            username = findProperty("DUSTREAN_REPO_USERNAME") as String? ?: System.getenv("DUSTREAN_REPO_USERNAME")
+            password = findProperty("DUSTREAN_REPO_PASSWORD") as String? ?: System.getenv("DUSTREAN_REPO_PASSWORD")
+        }
+    }
+    maven("https://repo.dustrean.net/releases") {
+        credentials {
+            username = findProperty("DUSTREAN_REPO_USERNAME") as String? ?: System.getenv("DUSTREAN_REPO_USERNAME")
+            password = findProperty("DUSTREAN_REPO_PASSWORD") as String? ?: System.getenv("DUSTREAN_REPO_PASSWORD")
+        }
+    }
 }
 
 dependencies {
     fun pluginDep(id: String, version: String) = "${id}:${id}.gradle.plugin:${version}"
     val kotlinVersion = "1.7.22"
+
+    implementation(pluginDep("net.dustrean.libloader", "1.3.3"))
 
     compileOnly(kotlin("gradle-plugin", embeddedKotlinVersion))
     runtimeOnly(kotlin("gradle-plugin", kotlinVersion))
