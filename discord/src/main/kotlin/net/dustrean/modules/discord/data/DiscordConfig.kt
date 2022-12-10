63e30eb585f73a5cbc00fc0436b1482e51841eab
Anton <anton.wiblishauser@gmx.de>
2022-12-10 22:55:22 +0100

    reformat discord config

--- discord/src/main/kotlin/net/dustrean/modules/discord/data/DiscordConfig.kt
+++ discord/src/main/kotlin/net/dustrean/modules/discord/data/DiscordConfig.kt
@@ -4,9 +4,19 @@ data class DiscordConfig(
     val token: String = "xxxxxxxxxxxxxxxxxxxxx",
     val teamDiscordID: String = "938425677345079326",
     val publicDiscordID: String = "789578052924866590",
+    val channels: Channels = Channels(),
+    val roles: Roles = Roles(),
+    val ruleMessages: List<Rule> = listOf()
+)
+
+data class Channels(
     val announcementChannelID: String = "",
     val teamAnnouncementCreateChannelID: String = "",
     val bugsChannelID: String = "",
     val closedTicketCategoryID: String = "",
     val openedTicketCategoryID: String = ""
 )
+
+data class Roles(val playerID: String = "")
+
+data class Rule(val color: Int = 0xFFFFF, val title: String = "", val description: String = "")
