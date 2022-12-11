1732133394ac7b782243f62ae63db51b53fc9ef7
Anton <79146501+btwonion@users.noreply.github.com>
2022-12-11 19:49:33 +0100

    remove token from DiscordConfig

--- discord/src/main/kotlin/net/dustrean/modules/discord/data/DiscordConfig.kt
+++ discord/src/main/kotlin/net/dustrean/modules/discord/data/DiscordConfig.kt
@@ -1,7 +1,6 @@
 package net.dustrean.modules.discord.data
 
 data class DiscordConfig(
-    val token: String = "xxxxxxxxxxxxxxxxxxxxx",
     val teamDiscordID: String = "938425677345079326",
     val publicDiscordID: String = "789578052924866590",
     val channels: Channels = Channels(),
