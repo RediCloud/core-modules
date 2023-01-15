ecc0fe0f53e57c78d4cb38225b02281764d535e2
Suqatri <paul@suqatri.net>
2023-01-15 13:44:57 +0100

    change openai api log level

--- discord/src/main/kotlin/net/dustrean/modules/discord/part/impl/moderation/ChatModerationPart.kt
+++ discord/src/main/kotlin/net/dustrean/modules/discord/part/impl/moderation/ChatModerationPart.kt
@@ -1,13 +1,18 @@
 package net.dustrean.modules.discord.part.impl.moderation
 
+import com.aallam.openai.api.logging.LogLevel
 import com.aallam.openai.api.moderation.ModerationRequest
 import com.aallam.openai.api.moderation.ModerationResult
 import com.aallam.openai.client.OpenAI
 import com.aallam.openai.client.OpenAIConfig
+import dev.kord.common.Color
+import dev.kord.core.behavior.channel.createMessage
+import dev.kord.core.entity.Message
 import dev.kord.core.event.message.MessageCreateEvent
+import dev.kord.core.event.message.MessageUpdateEvent
 import dev.kord.core.on
+import dev.kord.rest.builder.message.create.embed
 import net.dustrean.modules.discord.configManager
-import net.dustrean.modules.discord.data.DiscordConfig
 import net.dustrean.modules.discord.kord
 import net.dustrean.modules.discord.mainGuild
 import net.dustrean.modules.discord.part.DiscordModulePart
@@ -22,7 +27,7 @@ class ChatModerationPart() : DiscordModulePart() {
     private val cache = mutableMapOf<String, List<ModerationResult>>()
 
     override suspend fun init() {
-        openAi = OpenAI(System.getenv("OPENAI_API_KEY"))
+        openAi = OpenAI(OpenAIConfig(System.getenv("OPENAI_API_KEY"), LogLevel.Info))
         config = if (!configManager.exists("discord-bot:chat-moderation")) {
             val config = ChatModerationConfig()
             configManager.createConfig(config)
@@ -32,29 +37,35 @@ class ChatModerationPart() : DiscordModulePart() {
         }
     }
 
-    private val chat = kord.on<MessageCreateEvent> {
+    private val chatModeration = kord.on<MessageCreateEvent> {
         if (message.getGuild() != mainGuild) return@on
         if (message.author?.isBot == true) return@on
-        if (!config.channels.contains(message.channelId.value.toLong())) return@on
+        if (!config.chatModeration.contains(message.channelId.value.toLong())) return@on
 
         if (cache.containsKey(message.content.lowercase())) {
             val list = cache[message.content.lowercase()]!!
-            val violations = getViolations(list)
-            if(violations.isNotEmpty()) {
-                message.channel.createMessage("Your message was deleted because it was flagged as ${violations.joinToString(", ")}")
-                message.delete("Message was flagged as ${violations.joinToString(", ")}")
-            }
+            flag(message, getViolations(list))
             return@on
         }
         val request = ModerationRequest(message.content)
         val result = openAi.moderations(request)
         val list = result.results
-        val violations = getViolations(list)
-        if(violations.isNotEmpty()) {
-            message.channel.createMessage("Your message was deleted because it was flagged as ${violations.joinToString(", ")}")
-            message.delete("Message was flagged as ${violations.joinToString(", ")}")
-        }
+        flag(message, getViolations(list))
         cache[message.content.lowercase()] = list
+    }
+
+    private suspend fun flag(message: Message, violations: Set<String>) {
+        if(violations.isEmpty()) return
+        val author = message.author ?: return
+        val dmChannel = author.getDmChannelOrNull() ?: return
+        dmChannel.createMessage {
+            embed {
+                title = "Chat Moderation | DustreanNET"
+                color = Color(250, 0, 0)
+                description = "Your messsage: ```${message.content}``` was flagged for the following ${if(violations.size == 1) "violation" else "violations"}: ${violations.joinToString(", ")}"
+            }
+        }
+        message.delete("Message was flagged as ${violations.joinToString(", ")}")
     }
 
     private fun getViolations(list: List<ModerationResult>): Set<String> {
@@ -71,6 +82,10 @@ class ChatModerationPart() : DiscordModulePart() {
             }
         }
         return violation
+    }
+
+    private val logEdits = kord.on<MessageUpdateEvent> {
+
     }
 
 }
