1115197e7be53838ddb3fb0f85dd03ba9bde50b3
Suqatri <paul@suqatri.net>
2023-01-15 14:01:52 +0100

    improve chat moderation & log edits

--- discord/src/main/kotlin/net/dustrean/modules/discord/part/impl/moderation/ChatModerationConfig.kt
+++ discord/src/main/kotlin/net/dustrean/modules/discord/part/impl/moderation/ChatModerationConfig.kt
@@ -3,7 +3,24 @@ package net.dustrean.modules.discord.part.impl.moderation
 import net.dustrean.api.config.IConfig
 
 class ChatModerationConfig : IConfig {
+
     override val key = "discord-bot:chat-moderation"
 
-    val channels = mutableSetOf(1063220865594036234L)
+    val autoModeration = true
+    val addNewChannelsToModeration = true
+    val moderateOtherBots = false
+    val chatModeration = mutableSetOf(1063220865594036234L)
+
+    val logEdits = true
+    val addNewChannelsToEdits = true
+    val logBotEdits = false
+    val logEditsInChannels = mutableSetOf(1063220865594036234L)
+
+    val logDeletes = true
+    val addNewChannelsToDeletes = true
+    val logDeletesByBots = false
+    val logDeletesInChannels = mutableSetOf(1063220865594036234L)
+
+    val logChannel = 1047935742971748382L
+
 }
--- discord/src/main/kotlin/net/dustrean/modules/discord/part/impl/moderation/ChatModerationPart.kt
+++ discord/src/main/kotlin/net/dustrean/modules/discord/part/impl/moderation/ChatModerationPart.kt
@@ -6,9 +6,12 @@ import com.aallam.openai.api.moderation.ModerationResult
 import com.aallam.openai.client.OpenAI
 import com.aallam.openai.client.OpenAIConfig
 import dev.kord.common.Color
+import dev.kord.core.behavior.channel.asChannelOf
 import dev.kord.core.behavior.channel.createMessage
 import dev.kord.core.entity.Message
+import dev.kord.core.entity.channel.TextChannel
 import dev.kord.core.event.message.MessageCreateEvent
+import dev.kord.core.event.message.MessageDeleteEvent
 import dev.kord.core.event.message.MessageUpdateEvent
 import dev.kord.core.on
 import dev.kord.rest.builder.message.create.embed
@@ -17,6 +20,7 @@ import net.dustrean.modules.discord.kord
 import net.dustrean.modules.discord.mainGuild
 import net.dustrean.modules.discord.part.DiscordModulePart
 import net.dustrean.modules.discord.util.commands.CommandBuilder
+import net.dustrean.modules.discord.util.toSnowflake
 
 class ChatModerationPart() : DiscordModulePart() {
 
@@ -38,7 +42,7 @@ class ChatModerationPart() : DiscordModulePart() {
     }
 
     private val chatModeration = kord.on<MessageCreateEvent> {
-        if (message.author?.isBot == true) return@on
+        if (message.author?.isBot == true && !config.moderateOtherBots) return@on
         if (!config.chatModeration.contains(message.channelId.value.toLong())) return@on
 
         if (cache.containsKey(message.content.lowercase())) {
@@ -84,7 +88,30 @@ class ChatModerationPart() : DiscordModulePart() {
     }
 
     private val logEdits = kord.on<MessageUpdateEvent> {
-        
+        if (!config.logEditsInChannels.contains(message.channelId.value.toLong())) return@on
+        if (message.asMessage().author?.isBot == true && !config.logBotEdits) return@on
+
+        val oldMessage = old?.asMessageOrNull() ?: return@on
+        val newMessage = message.asMessageOrNull() ?: return@on
+
+        val logChannel = mainGuild.getChannelOrNull(config.logChannel.toSnowflake) ?: return@on
+
+        logChannel.asChannelOf<TextChannel>().createMessage {
+            embed {
+                title = "Message Edited | DustreanNET"
+                color = Color(250, 0, 0)
+                url = "https://discord.com/channels/${message.asMessage().getGuildOrNull()?.id?.value}/${message.channelId.value}/${message.id.value}"
+                description = "Message edited in ${message.channel.mention} by ${message.asMessage().author?.mention}"
+                field {
+                    name = "Old Content"
+                    value = oldMessage.content
+                }
+                field {
+                    name = "New Content"
+                    value = newMessage.content
+                }
+            }
+        }
     }
 
 }
