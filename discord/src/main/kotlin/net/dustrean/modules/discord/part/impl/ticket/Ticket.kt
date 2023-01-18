6179ce068b7c471db4c947e7dba1d75f46a08881
Suqatri <paul@suqatri.net>
2023-01-19 00:09:17 +0100

    add TicketStates, state history and uniqueid for each ticket

--- discord/src/main/kotlin/net/dustrean/modules/discord/part/impl/ticket/Ticket.kt
+++ discord/src/main/kotlin/net/dustrean/modules/discord/part/impl/ticket/Ticket.kt
@@ -1,10 +1,29 @@
 package net.dustrean.modules.discord.part.impl.ticket
 
+import java.util.*
+
 class Ticket {
 
-    var creatorId = 0L
+    var id = UUID.randomUUID()
     var channelId = 0L
-    var open = false
+    var stateHistory = mutableMapOf<Long, Pair<TicketState, Long>>()
     var lastCreatorMessage = 0L
 
+    fun isOpen(): Boolean =
+        stateHistory.values.last().first == TicketState.OPENED || stateHistory.values.last().first == TicketState.REOPENED
+
+    val creatorId: Long
+        get() = stateHistory.values.first { it.first == TicketState.OPENED }.second
+
+    fun update() {
+        TicketPart.tickets[id] = this
+    }
+
+}
+
+enum class TicketState {
+    OPENED,
+    CLOSED,
+    REOPENED,
+    DELETED
 }
