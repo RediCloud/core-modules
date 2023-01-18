5f05c6b3e7a1e1c6a445944085297b03f9c6f11d
Suqatri <paul@suqatri.net>
2023-01-19 00:07:23 +0100

    improve dc configs

--- discord/src/main/kotlin/net/dustrean/modules/discord/part/impl/rule/RuleConfig.kt
+++ discord/src/main/kotlin/net/dustrean/modules/discord/part/impl/rule/RuleConfig.kt
@@ -1,13 +1,13 @@
 package net.dustrean.modules.discord.part.impl.rule
 
 import net.dustrean.modules.discord.data.AbstractDiscordConfig
+import net.dustrean.modules.discord.data.Emoji
 
 class RuleConfig() : AbstractDiscordConfig() {
     override val key: String = "discord:modules:rule"
     val ruleMessages: List<Rule> = listOf()
     var acceptEmoji: Emoji = Emoji(null, "✅")
     var acceptRole = 1064179288552787969L
-    val acceptMessages = mutableListOf<Long>()
 }
 
 data class Rule(val color: Int = 0xFFFFF, val title: String = "", val description: String = "")
