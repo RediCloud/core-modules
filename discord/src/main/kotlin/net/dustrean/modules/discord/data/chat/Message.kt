4a94271a57500ae250ece5e0414abc864507c491
Suqatri <paul@suqatri.net>
2023-01-23 22:22:58 +0100

    switched from old message format to new format

--- discord/src/main/kotlin/net/dustrean/modules/discord/data/chat/Message.kt
+++ discord/src/main/kotlin/net/dustrean/modules/discord/data/chat/Message.kt
@@ -16,32 +16,33 @@ data class Message(
 
 fun message(block: Message.() -> Unit): Message = Message().apply(block)
 
-//Json from https://old.message.style/dashboard
+//Json from https://message.style
 fun toMessage(json: String): Message {
     val element = JsonParser().parse(json)
     val message = Message()
-    message.content = element.asJsonObject["content"].asString
+    message.content = element.asJsonObject["content"]?.asString
     element.asJsonArray.forEach { embedJsonElement ->
         val embed = Embed()
-        embed.title = embedJsonElement.asJsonObject["title"].asString
+        embed.title = embedJsonElement.asJsonObject["title"]?.asString
         //embed.color = embedJsonElement.asJsonObject["color"].asInt //TODO
-        embed.description = embedJsonElement.asJsonObject["description"].asString
-        embed.url = embedJsonElement.asJsonObject["url"].asString
+        embed.description = embedJsonElement.asJsonObject["description"]?.asString
+        embed.url = embedJsonElement.asJsonObject["url"]?.asString
         embedJsonElement.asJsonObject.also {
-            embed.author = it["name"].asString
-            embed.authorUrl = it["url"].asString
-            embed.authorIconUrl = it["icon_url"].asString
+            embed.author = it["name"]?.asString
+            embed.authorUrl = it["url"]?.asString
+            embed.authorIconUrl = it["icon_url"]?.asString
         }
-        embed.imageUrl = embedJsonElement.asJsonObject["image"].asJsonObject["url"].asString
-        embed.thumbnailUrl = embedJsonElement.asJsonObject["thumbnail"].asJsonObject["url"].asString
-        embedJsonElement.asJsonObject["footer"].also {
-            embed.footer = it.asJsonObject["text"].asString
-            embed.footerIconUrl = it.asJsonObject["icon_url"].asString
+        embed.imageUrl = embedJsonElement.asJsonObject["image"]?.asJsonObject?.get("url")?.asString
+        embed.thumbnailUrl = embedJsonElement.asJsonObject["thumbnail"]?.asJsonObject?.get("url")?.asString
+        embedJsonElement.asJsonObject["footer"]?.also {
+            embed.footer = it.asJsonObject["text"]?.asString
+            embed.footerIconUrl = it.asJsonObject["icon_url"]?.asString
         }
-        embedJsonElement.asJsonObject["fields"].asJsonArray.forEach { fieldJsonElement ->
-            val key = fieldJsonElement.asJsonObject["name"].asString
-            val value = fieldJsonElement.asJsonObject["value"].asString
-            val inline = fieldJsonElement.asJsonObject["inline"].asBoolean
+        embedJsonElement.asJsonObject["fields"]?.asJsonArray?.forEach { fieldJsonElement ->
+            val key = fieldJsonElement.asJsonObject["name"]?.asString
+            val value = fieldJsonElement.asJsonObject["value"]?.asString
+            val inline = fieldJsonElement.asJsonObject["inline"]?.asBoolean ?: false
+            if (key == null || value == null) return@forEach
             val field = EmbedField(key, value, inline)
             embed.fields.add(field)
         }
