c0412c50cba94dffbdd0443205a64da124f6a8d7
Suqatri <paul@suqatri.net>
2023-01-24 22:17:37 +0100

    create stop command & status notify

---
+++ discord/src/main/kotlin/net/dustrean/modules/discord/commands/StopCommand.kt
@@ -0,0 +1,91 @@
+package net.dustrean.modules.discord.commands
+
+import dev.kord.common.entity.Permission
+import dev.kord.common.entity.Permissions
+import dev.kord.common.entity.Snowflake
+import dev.kord.core.behavior.channel.createMessage
+import dev.kord.core.behavior.interaction.respondEphemeral
+import dev.kord.core.entity.User
+import dev.kord.rest.builder.message.create.embed
+import kotlinx.coroutines.launch
+import net.dustrean.api.ICoreAPI
+import net.dustrean.modules.discord.DiscordModuleMain
+import net.dustrean.modules.discord.ioScope
+import net.dustrean.modules.discord.logChannel
+import net.dustrean.modules.discord.part.parts
+import net.dustrean.modules.discord.util.message.useDefaultDesign
+
+class StopCommand() : AbstractInputCommand("stop", null, "Stop the bot") {
+
+    init {
+        permissions = Permissions(Permission.All)
+        perform {
+            ioScope.launch {
+                interaction.respondEphemeral {
+                    embed {
+                        title = "Stopping | DustreanNET"
+                        description = "Stopping the bot..."
+                        useDefaultDesign(interaction.user)
+                    }
+                }
+                notifyStop(interaction.user)
+                DiscordModuleMain.INSTANCE.onDisable(ICoreAPI.INSTANCE)
+            }
+        }
+    }
+
+}
+
+val started = System.currentTimeMillis()
+var stopped = false
+
+suspend fun notifyStart() {
+    logChannel.createMessage {
+        embed {
+            title = "Status | DustreanNET"
+            description = ":green_square: State: Started\n" +
+                    ":electric_plug: Version: ${ICoreAPI.INSTANCE.getCoreVersion()}\n" +
+                    ":calendar: Date: ${
+                        java.time.LocalDateTime.now().format(
+                            java.time.format.DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss")
+                        )
+                    }\n" +
+                    ":chains: Enabled parts: ${
+                        parts.map { it.name }.joinToString(", ")
+                    }" +
+                    ":globe_with_meridians: Hostname: ${
+                        if (System.getenv().containsKey("CORE_HOSTNAME")) {
+                            System.getenv("CORE_HOSTNAME")
+                        } else {
+                            "Unknown"
+                        }
+                    }"
+        }
+    }
+}
+
+suspend fun notifyStop(user: User? = null) {
+    if (stopped) return
+    stopped = true
+    logChannel.createMessage {
+        embed {
+            title = "Status | DustreanNET"
+            description = ":red_square: State: Stopping\n" +
+                    ":calendar: Date: ${
+                        java.time.LocalDateTime.now().format(
+                            java.time.format.DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss")
+                        )
+                    }\n" +
+                    ":clock1: Uptime: ${
+                        java.time.Duration.ofMillis(System.currentTimeMillis() - started).toHours()
+                    } minutes\n" +
+                    ":globe_with_meridians: Hostname: ${
+                        if (System.getenv().containsKey("CORE_HOSTNAME")) {
+                            System.getenv("CORE_HOSTNAME")
+                        } else {
+                            "Unknown"
+                        }
+                    }"
+        }
+    }
+}
