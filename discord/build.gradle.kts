2ad5547bea4c5c4080680f56b8e0c1cd7abd6de4
Suqatri <paul@suqatri.net>
2023-04-08 11:54:22 +0200

    delete discord build script

--- discord/build.gradle.kts
+++ discord/build.gradle.kts
@@ -1,4 +1,13 @@
 plugins {
     `kotlin-script`
-    `discord-script`
+    kotlin("jvm")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("dev.kord:kord-core:${BuildConstants.kordVersion}")
+    implementation("com.aallam.openai:openai-client:2.1.2")
 }
