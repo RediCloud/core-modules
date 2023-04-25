41fbd9df078f37616f5d9d4e6916d95bd8e3b65f
Suqatri <paul@suqatri.net>
2023-04-25 17:47:26 +0200

    add plattform-clients to gradle scripts

--- buildSrc/src/main/kotlin/paper-script.gradle.kts
+++ buildSrc/src/main/kotlin/paper-script.gradle.kts
@@ -5,10 +5,14 @@ plugins {
 
 repositories {
     mavenCentral()
+    maven("https://repo.redicloud.dev/snapshots")
+    maven("https://repo.redicloud.dev/releases")
 }
 
 dependencies {
     paperDevBundle("${BuildConstants.minecraftVersion}-R0.1-SNAPSHOT")
+
+    implementation("dev.redicloud.clients:paper:1.0.0-SNAPSHOT")
 
     implementation("net.kyori:adventure-api:${BuildConstants.adventureVersion}")
     implementation("net.kyori:adventure-text-minimessage:${BuildConstants.adventureVersion}")
--- buildSrc/src/main/kotlin/velocity-script.gradle.kts
+++ buildSrc/src/main/kotlin/velocity-script.gradle.kts
@@ -1,11 +1,18 @@
+import gradle.kotlin.dsl.accessors._5b841d749b44c4d634f7cfea3ed45134.implementation
+
 plugins {
     kotlin("jvm")
 }
 
 repositories {
+    mavenCentral()
     maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://repo.redicloud.dev/snapshots")
+    maven("https://repo.redicloud.dev/releases")
 }
 
 dependencies {
     implementation("com.velocitypowered:velocity-api:${BuildConstants.velocityAPIVersion}")
+
+    implementation("dev.redicloud.clients:velocity:1.0.0-SNAPSHOT")
 }
